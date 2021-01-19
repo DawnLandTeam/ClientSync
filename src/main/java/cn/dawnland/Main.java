acbabe75564275b42a1aedb8d542249f7604a69a
MaoZa <cap_sub@dawnland.cn>
2021-01-19 18:04:32 +0800

    执行完成后启动HMCL 添加删除文件更新操作

--- src/main/java/cn/dawnland/Main.java
+++ src/main/java/cn/dawnland/Main.java
@@ -1,45 +1,40 @@
 package cn.dawnland;
 
-import java.io.*;
-import java.nio.charset.Charset;
-import java.nio.file.CopyOption;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 import cn.dawnland.data.ExecuteResult;
 import cn.dawnland.utils.HttpUtils;
 import cn.dawnland.utils.VersionUtils;
 import cn.dawnland.vo.VersionResponse;
-import org.apache.commons.codec.binary.StringUtils;
-import org.apache.http.client.HttpClient;
-import sun.nio.cs.ext.GBK;
 
-import javax.xml.transform.Source;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
     private static final List<VersionResponse> versions = VersionUtils.getVersions();
     private static final File dataFile = new File("version.dat");
+    private static final String prefix = "[黎明娘] - ";
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        System.out.println("初始化完成");
+        print("初始化完成");
         ObjectInputStream ois = null;
         try {
             FileInputStream fis = new FileInputStream(dataFile);
             ois = new ObjectInputStream(fis);
         } catch (Exception e) {
-            System.out.println("未找到历史执行记录，将按照基准执行");
+            print("未找到历史执行记录，将按照基准执行");
         }
         List<ExecuteResult> results = dataFile.exists() ? (List<ExecuteResult>) ois.readObject() : new ArrayList<>();
         List<Integer> beenExecutedVersion = results.stream().map(ExecuteResult::getNumberVersion).collect(Collectors.toList());
         List<VersionResponse> untreated = versions.stream().filter(v -> !beenExecutedVersion.contains(v.getNumberVersion())).collect(Collectors.toList());
         untreated.forEach(v -> {
+            print("正在执行" + v.getDisplayVersion() + "更新");
             if(v.getFiles() != null && v.getFiles().size() > 0){
                 v.getFiles().forEach((key, value) -> {
                     String[] split = key.split("/");
@@ -56,6 +51,7 @@ public class Main {
             }
             try {
                 Process exec = Runtime.getRuntime().exec("cmd.exe /c " + v.getCommand());
+                print(v.getDescription());
                 BufferedReader bf = new BufferedReader(new InputStreamReader(exec.getInputStream(), Charset.forName("GBK")));
                 StringBuffer sb = new StringBuffer();
                 String line = bf.readLine();
@@ -72,11 +68,20 @@ public class Main {
             } catch (Exception e) {
                 e.printStackTrace();
             }
+            print(v.getDisplayVersion() + "更新成功");
         });
         try(FileOutputStream fos = new FileOutputStream(dataFile);
             ObjectOutputStream oos = new ObjectOutputStream(fos)){
             oos.writeObject(results);
         }
+        // TODO: 2021-01-19 调起HMCL
+        print("按回车启动");
+        new Scanner(System.in).nextLine();
+        new ProcessBuilder("启动器.exe").start();
+    }
+
+    private static void print(String text){
+        System.out.println(prefix + text);
     }
 
 }
